622e72a2aedfc7e70901f80725fafe8b0c8be9a4
erivan123erivan <96080412+erivan123erivan@users.noreply.github.com>
2023-07-30 22:30:45 +0200

    Update server.js
    new

--- server.js
+++ server.js
@@ -36,41 +36,24 @@ app.post("/pay", async (req, res) => {
 
 
 app.post("/transfer", async (req, res) => {
-  const { name, totalPrice, pickupDate, returnDate, swiftCode, iban } = req.body;
-
   try {
-    // Create a PaymentIntent to charge the customer
+    const { name, totalPrice,  pickupDate, returnDate } = req.body; // Extract additional data from the request body
+    if (!name || !totalPrice || !pickupDate || !returnDate) {
+      return res.status(400).json({ message: "Please provide all required data" });
+    }
+
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: totalPrice * 100, // Stripe accepts amounts in cents
-      currency: "eur", // Change to your preferred currency
+      amount: Math.round(totalPrice * 100), // Convert totalPrice to the lowest currency unit (e.g., cents)
+      currency: "eur",
       payment_method_types: ["card"],
-      metadata: {
-        pickupDate,
-        returnDate,
-        name,
-        swiftCode,
-        iban,
-      },
+      metadata: { name, pickupDate, returnDate }, // Include additional data in the metadata
     });
 
-    // Handle the payment (charge the customer's card)
-    await stripe.paymentIntents.confirm(paymentIntent.id);
-
-    // Initiating the transfer to the specified IBAN and Swift code account
-    const transfer = await stripe.transfers.create({
-      amount: totalPrice * 100, // Stripe accepts amounts in cents
-      currency: "eur", // Change to your preferred currency
-      destination: {
-        type: "bank_account",
-        account: iban,
-      },
-    });
-
-    console.log("Transfer success:", transfer);
-    res.status(200).json({ message: "Payment and transfer successful!" });
-  } catch (error) {
-    console.error("Error during payment and transfer:", error);
-    res.status(500).json({ message: "Payment and transfer failed. Please try again later." });
+    const clientSecret = paymentIntent.client_secret;
+    res.json({ message: "Payment initiated", clientSecret });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
